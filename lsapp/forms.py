f1ca967bc877190152d14bc4d83ffa9256fb18c4
chisei00 <genesisnaldoza@gmail.com>
2025-05-15 18:48:13 +0800

    fix: login and register now func, registers at superuser

--- lsapp/forms.py
+++ lsapp/forms.py
@@ -2,11 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 
-class UserRegistrationForm(UserCreationForm):
+class RegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=101)
     last_name = forms.CharField(max_length=101)
-    email = forms.EmailField()
-
+    
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1','password2')
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1','password2']
--- lsapp/views.py
+++ lsapp/views.py
@@ -2,26 +2,40 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-from .forms import UserRegistrationForm
+from .forms import RegistrationForm
 
 
 def home(request):
     return render(request, 'users/index.html')
 
 def register(request):
+    form = RegistrationForm()
+
     if request.method == 'POST':
-       form = UserRegistrationForm(request.POST)
-       if form.is_valid():
-               form.save()
-
-               messages.success(request, f'Your account has been created. You can login now!')
-               return redirect('login')
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+                form.save()
+                
+                messages.success(request, f'Your account has been created. You can login now!')
+                return redirect('login')
     else:
-        form = UserRegistrationForm()
+         form = RegistrationForm()
 
-        context = {'form' : form}
-        return render(request, 'users/register.html', context)
-        
+    context = {'form' : form}
+    return render(request, 'users/register.html', context)
+
+#    if request.method == 'POST':
+#       form = RegistrationForm(request.POST)
+#       if form.is_valid():
+#               form.save()
+
+               #messages.success(request, f'Your account has been created. You can login now!')
+#               return render('users/register.html')
+#    else:
+#        form = RegistrationForm()
+
+#        context = {'form' : form}
+#        return render(request, 'users/register.html', context)
 
 
 # Create your views here.
